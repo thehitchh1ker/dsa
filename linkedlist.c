8cdec18593d63434118e399a73f3606e21104e2e
Kumar Aditya <42440150+thehitchh1ker@users.noreply.github.com>
2023-06-29 20:40:49 +0530

    function to reverse list

--- linkedlist.c
+++ linkedlist.c
@@ -56,8 +56,7 @@ void delete_front(Node** head_ref) {
 void delete_end(Node** tracer) {
     if (*tracer && (*tracer)->next) {
         delete_end(&(*tracer)->next);
-    }
-    else {
+    } else {
         free(*tracer);
         *tracer = NULL;
     }
@@ -81,67 +80,28 @@ void delete_list(Node** tracer) {
     *tracer = NULL;
 }
 
+Node* reverse(Node* head) {
+    if (!head || !head->next) return head;
+    Node* rest = reverse(head->next);
+    head->next->next = head;
+    head->next = NULL;
+
+    return rest;
+}
+
 int main() {
     Node* head = NULL;
-    // int arr[] = { 1, 3, 4, 4, 5};
-    // int len = sizeof(arr)/sizeof(arr[0]);
-    // for (int i = len-1; i >= 0; i--) {
-    //     insert_front(&head, arr[i]);
-    // }
+    int arr[] = {-5, 1, 3, 2, 2, 7, 2};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    for (int i = len - 1; i >= 0; i--) {
+        insert_front(&head, arr[i]);
+    }
 
-    int choice, val;
-    do {
-        printf("\n************\n");
-        printf("1. Print list\n");
-        printf("2. Insert node at front\n");
-        printf("3. Insert node before the first larger number\n");
-        printf("4. Insert node at end\n");
-        printf("5. Delete node at front\n");
-        printf("6. Delete node at end\n");
-        printf("7. Delete the first occurence of a node\n");
-        printf("8. EXIT");
-        printf("\n************\n");
-
-        printf("\n Enter your choice: ");
-        scanf("%d", &choice);
-        switch (choice) {
-            case 1:
-                printf("\nList: ");
-                print_list(head);
-                break;
-            case 2:
-                printf("Enter value: ");
-                scanf("%d", &val);
-                insert_front(&head, val);
-                break;
-            case 3:
-                printf("Enter value: ");
-                scanf("%d", &val);
-                insert(&head, val);
-                break;
-            case 4:
-                printf("Enter value: ");
-                scanf("%d", &val);
-                insert_end(&head, val);
-                break;
-            case 5:
-                delete_front(&head);
-                break;
-            case 6:
-                delete_end(&head);
-                break;
-            case 7:
-                printf("Enter value: ");
-                scanf("%d", &val);
-                delete (&head, val);
-                break;
-            case 8:
-                break;
-            default:
-                printf("\n Wrong Choice");
-                break;
-        }
-    } while (choice != 8);
+    // head = create_node(4);
+    // head->next = create_node(2);
+    print_list(head);
+    head = reverse(head);
+    print_list(head);
 
     delete_list(&head);
 }
